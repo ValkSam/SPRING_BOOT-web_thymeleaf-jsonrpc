25e71c71628634944648a3419025abd60e3cb716
ValkSam <do110473sva@gmail.com>
2017-06-18 01:47:42 +0300

    VER 1.2

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,7 +2,8 @@ var stompClient = null;
 
 function connect() {
 //        var socket = new SockJS('/v1/order');
-    var socket = new SockJS("/v1/order?jsonrpc=2.0&id=1&method=get&params=%7B%22uid%22%3A2%7D");
+    var socket = new SockJS("/v1/order?jsonrpc=2.0&id=1&method=getAll");
+    // var socket = new SockJS("/v1/order?jsonrpc=2.0&id=1&method=get&params=%7B%22id%22%3A2%7D");
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
@@ -22,8 +23,8 @@ function disconnect() {
 }
 
 connect();
-stompClient.send(JSON.stringify({"id":"1","jsonrpc":"2.0","method":"get","params":{"id":2}}));
-//    stompClient.send("test");
+// stompClient.send(JSON.stringify({"id":"1","jsonrpc":"2.0","method":"get","params":{"id":2}}));
+   stompClient.send("test");
 //    stompClient.send("/", {}, JSON.stringify({"id":"1","jsonrpc":"2.0","method":"get","params":{"id":2}}));
 //    stompClient.send("/", {}, JSON.stringify({"id":"1","jsonrpc":"2.0","method":"get","params":{"id":2}}));
 disconnect();
